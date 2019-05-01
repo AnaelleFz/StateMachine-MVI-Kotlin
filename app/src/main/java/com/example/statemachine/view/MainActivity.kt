cb08ebe6d968ff639ce16fcb2a82a0327f5c51fa
Anaëlle <anaelle.fievez@protonmail.com>
2019-05-01 20:55:17 +0200

    FEAT : Add new button on view and manage button visibility according to the state.

--- app/src/main/java/com/example/statemachine/view/MainActivity.kt
+++ app/src/main/java/com/example/statemachine/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.statemachine.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.statemachine.R
 import com.example.statemachine.model.MainPresenter
@@ -35,32 +36,58 @@ class MainActivity : AppCompatActivity() {
 
     private fun renderStopState(dataState: State.StopState) {
         text_state.setText(R.string.stop_state)
+        btn_alert.visibility = View.INVISIBLE
+        btn_stop.visibility = View.INVISIBLE
+        btn_start.visibility = View.VISIBLE
+        btn_error.visibility = View.VISIBLE
+        btn_close.visibility = View.INVISIBLE
     }
 
     private fun renderStartState(dataState: State.StartState) {
         text_state.setText(R.string.start_state)
+        btn_alert.visibility = View.VISIBLE
+        btn_stop.visibility = View.VISIBLE
+        btn_start.visibility = View.INVISIBLE
+        btn_error.visibility = View.VISIBLE
+        btn_close.visibility = View.INVISIBLE
     }
 
     private fun renderInitState(dataState: State.InitState) {
         text_state.setText(R.string.init_state)
+        btn_alert.visibility = View.INVISIBLE
+        btn_stop.visibility = View.INVISIBLE
+        btn_start.visibility = View.INVISIBLE
+        btn_error.visibility = View.VISIBLE
+        btn_close.visibility = View.INVISIBLE
     }
 
     private fun renderErrorState(dataState: State.ErrorState) {
         text_state.setText(R.string.error_state)
+        btn_alert.visibility = View.INVISIBLE
+        btn_stop.visibility = View.VISIBLE
+        btn_start.visibility = View.INVISIBLE
+        btn_error.visibility = View.INVISIBLE
+        btn_close.visibility = View.INVISIBLE
     }
 
     private fun renderAlertState(dataState: State.AlertState) {
         text_state.setText(R.string.alert_state)
+        text_state.setText(R.string.error_state)
+        btn_alert.visibility = View.INVISIBLE
+        btn_stop.visibility = View.INVISIBLE
+        btn_start.visibility = View.INVISIBLE
+        btn_error.visibility = View.INVISIBLE
+        btn_close.visibility = View.INVISIBLE
     }
 
-    fun displayStopIntent() = btn_stop.clicks()
+    fun stopEventIntent() = btn_stop.clicks()
 
-    fun displayStartIntent() = btn_start.clicks()
+    fun startEventIntent() = btn_start.clicks()
 
-    fun displayErrorIntent() = btn_error.clicks()
+    fun errorEventIntent() = btn_error.clicks()
 
-    fun displayAlertIntent() = btn_alert.clicks()
+    fun alertEventItent() = btn_alert.clicks()
 
-    fun displayInitIntent() = btn_init.clicks()
+    fun closeEventItent() = btn_close.clicks()
 
 }
