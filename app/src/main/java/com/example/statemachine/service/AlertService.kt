e80f729e8b98791d7994271f27d9f61f4d8f390e
Anaëlle <anaelle.fievez@protonmail.com>
2019-05-04 14:53:52 +0200

    FEAT : Bind service and activity.

--- app/src/main/java/com/example/statemachine/service/AlertService.kt
+++ app/src/main/java/com/example/statemachine/service/AlertService.kt
@@ -2,11 +2,32 @@ package com.example.statemachine.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import com.example.statemachine.model.statemachine.EventEnum
+import io.reactivex.Observer
 
 class AlertService : Service() {
 
+    private val alertServiceBinder = AlertServiceBinder()
+
+    private lateinit var alertObserver: Observer<EventEnum>
+
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return alertServiceBinder
+    }
+
+    override fun onStart(intent: Intent?, startId: Int) {
+        super.onStart(intent, startId)
+    }
+
+    fun setAlertObserver(alertObserver: Observer<EventEnum>) {
+        this.alertObserver = alertObserver
+    }
+
+    inner class AlertServiceBinder : Binder() {
+        fun getService(): AlertService {
+            return this@AlertService
+        }
     }
 }
